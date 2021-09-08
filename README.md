3ac4a917774473c1ffc6d558e07b49873527fe4b
KanzakiMiner-Offical <kietgaming28@gmail.com>
2021-09-08 19:54:14 +0700

    Add Animal Baby Seperator/Rewrie README.md

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Industrial-Foregoing-PE
 Machine,Auto,Energy,more.....
+
+
+
+[Industrial-Foregoing-PE](https://icmods.mineprogramming.org/mod?id=4666) is a version port form Industrial-Foregoing-PC
+
+
+Mod has api from IC2,so I thank you MineExplore because you lent me the code
+
+Original copyright belongs to hrznstudio
+
+## 
+
+* First Version [![](http://img.youtube.com/vi/2QGjqTwBePs/0.jpg)](https://youtu.be/2QGjqTwBePs)
+* Comming Soon
+
+## End
+
+I hope You can contact me to complete it :D
+
--- dev/blocks/machines/abs.js
+++ dev/blocks/machines/abs.js
@@ -18,7 +18,20 @@ Block.createBlock("animal_separator", [
 
 
 var AnimalBaby = [];
-var AnimalGrow = [];
+var AnimalGrow = [
+    EntityType.CAT,
+    EntityType.COD,
+    EntityType.COW,
+    EntityType.CHICKEN,
+    EntityType.CREEPER,
+    EntityType.DONKEY,
+    EntityType.HORSE,
+    EntityType.LLAMA,
+    EntityType.PANDA,
+    EntityType.PIG,
+    EntityType.SHEEP,
+    EntityType.TURTLE
+  ];
 
 
 var guiABS = new UI.StandartWindow({
@@ -40,3 +53,89 @@ var guiABS = new UI.StandartWindow({
   }
 });
 
+MachineRegistry.registerElectricMachine(BlockID.mob_crusher, {
+
+  defaultValues: {
+
+    progress: 0,
+    power_tier: 1,
+    energy_storage: 51000,
+    energy_consumption: 40,
+    work_time: 25,
+    minX: 0,
+    minY: 0,
+    minZ: -1,
+    maxX: 0,
+    maxY: 1,
+    maxZ: 0,
+    scanZ: 0,
+    scanY: 0,
+    scanX: 0
+  },
+
+  getGuiScreen: function() {
+    return guiMC;
+  },
+
+  getTier: function() {
+    return this.data.power_tier;
+  },
+
+  tick: function() {
+    StorageInterface.checkHoppers(this);
+
+    if (this.data.energy >= this.data.energy_consumption) {
+      this.scan();
+    }
+
+    var energyStorage = this.getEnergyStorage();
+    this.data.energy = Math.min(this.data.energy, energyStorage);
+    //     this.data.energy += ChargeItemRegistry.getEnergyFrom(this.container.getSlot("slotEnergy"), "RF", energyStorage - this.data.energy, this.getTier());
+
+    this.container.setScale("energyScale", this.data.energy / energyStorage);
+    this.container.setScale("progressScale", this.data.progress);
+  },
+
+  scan: function() {
+    this.data.scanX++;
+    if (this.data.scanX > this.data.maxX) {
+      this.data.scanX = this.data.minX;
+      this.data.scanZ++;
+      if (this.data.scanZ > this.data.maxZ) {
+        this.data.scanZ = this.data.maxZ;
+        this.data.scanY++;
+        if (this.data.scanY > this.data.maxY) {
+          this.data.scanY = this.data.maxY;
+        }
+      }
+    }
+
+    for (i in AnimalGrow) {
+      var AG = AnimalGrow[i];
+      let ANIMAL_ARR = Entity.getAllInRange(this.x + scanZ, this.y + scanY, this.z + scanZ, 0);
+
+      for (let i in ANIMAL_ARR) {
+        if (ENTITY_ARR[i] === AG) {
+          var AnimalAges = Entity.getAge(ENTITY_ARR[i]);
+          if (AnimalAges <= 0) {
+            this.data.progress += 1 / this.data.work_time;
+            if (this.data.progress >= 1) {
+              Entity.setPotion(ENTITY_ARR[i], this.x + 1, this.y, this.z + 1);
+              this.data.energy -= this.data.energy_consumption;
+              this.data.progress = 0;
+            } else {
+              this.data.progress = 0;
+            }
+          }
+        }
+      }
+    }
+
+  },
+
+  getEnergyStorage: function() {
+    return this.data.energy_storage;
+  },
+
+  energyReceive: MachineRegistry.basicEnergyReceiveFunc
+});
